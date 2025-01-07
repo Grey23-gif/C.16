1472b7f060666ecf46d7c3e92a29fe31c75d7ac0
Grey <duong230426@gmail.com>
2025-01-07 16:13:41 +0700

    Update 16.1.c

--- 16.1.c
+++ 16.1.c
@@ -8,11 +8,11 @@ int main()
     
     printf("C1:");
     printf("\nGai tri cua a: %d",a );
-    printf("\nDia chi cua a: %p",(void*)&a);
+    printf("\nDia chi cua a: %p",&a);
     
     printf("\nC2:");
     printf("\nGai tri cua a qua con tro: %d",*ptr );
-    printf("\nDia chi cua a qua con tro: %p",(void*)ptr);
+    printf("\nDia chi cua a qua con tro: %p",ptr);
 
     return 0;
 }
